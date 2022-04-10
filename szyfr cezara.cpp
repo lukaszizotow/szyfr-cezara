d6582ca053633ee344f9938d4783cd0cc7c1a031
Łukasz Izotow <lizotow476@technikumkreatywne.pl>
2022-04-10 18:20:30 +0200

    Dodaj pliki projektów.

---
+++ szyfr cezara.cpp
@@ -0,0 +1,25 @@
+﻿
+#include <iostream>
+
+int main()
+{
+	void szyfruj(int klucz, char tab[])
+	{
+		int dl = strlen(tab);
+		if (!(klucz >= -26 && klucz <= 26)) return;
+
+		if (klucz >= 0)
+			for (int i = 0; i < dl; i++)
+				if (tab[i] + klucz <= 'z')
+					tab[i] += klucz;
+				else
+					tab[i] = tab[i] + klucz - 26;
+		else
+			for (int i = 0; i < dl; i++)
+				if (tab[i] + klucz >= 'a')
+					tab[i] += klucz;
+				else
+					tab[i] = tab[i] + klucz + 26;
+	}
+    
+}
